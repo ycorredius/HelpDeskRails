4b222a6adcc62713cfb2c5e2a7f94e1f3f3ac117
shaun <ycorredius@gmail.com>
2020-02-01 11:34:44 -0500

    Basic models views and routes built

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,2 +1,21 @@
 class ResponsesController < ApplicationController
+    def new
+       @ticket = Ticket.find_by(id: params[:ticket_id]) 
+       @response = @ticket.responses.build
+    end
+
+    def create
+        @ticket = Ticket.find_by(id: params[:ticket_id]) 
+        @response = current_user.responses.build(response_params)
+        if @response.save
+            redirect_to ticket_path(@ticket)
+        else
+            render :new
+        end
+    end
+
+    private
+    def response_params
+        params.require(:response).permit(:content,:ticket_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'welcome#welcome'
 
-  get '/users/:id/profile', to: 'users#show'
-
   resources :users do
-    resources :tickets
+    resources :tickets 
   end
   resources :users do
     resources :responses
   end
-  
-  resources :tickets
-  resources :responses
+  resources :tickets , only: [:index,:show]
+  resources :tickets do
+    resources :responses
+  end
 end
