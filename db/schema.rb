1b48a23ec869ae62483130bb84fe9976ad06dd51
shaun <ycorredius@gmail.com>
2020-02-08 07:42:18 -0500

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_07_172200) do
+ActiveRecord::Schema.define(version: 2020_02_07_174645) do
 
   create_table "responses", force: :cascade do |t|
     t.string "content"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_02_07_172200) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.string "description"
+    t.string "status"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,6 +44,7 @@ ActiveRecord::Schema.define(version: 2020_02_07_172200) do
     t.string "uid"
     t.string "name"
     t.string "image"
+    t.boolean "is_admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
